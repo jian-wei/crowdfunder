cf151ac1cd1f50cb50fccfa18e909a7a4b661ec5
jian-wei <jianweichuah@gmail.com>
2014-07-17 13:24:43 +0800

    Fixed bug in index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  buf = fs.readFileSync(index.html);
+  buf = fs.readFileSync('index.html');
   response.send(buf.toString());
 });
 
